99d774bc8dda920ffb7a4c1a55146e3227b964f7
fivesmallq <fivesmallq@gmail.com>
2016-04-27 13:56:10 +0800

    add proxy provider interface

---
+++ src/main/java/im/nll/data/fluent/ProxyProvider.java
@@ -0,0 +1,15 @@
+package im.nll.data.fluent;
+
+/**
+ * @author <a href="mailto:fivesmallq@gmail.com">fivesmallq</a>
+ * @version Revision: 1.0
+ * @date 16/4/27 下午1:52
+ */
+public interface ProxyProvider {
+    /**
+     * return a proxy.
+     *
+     * @return
+     */
+    Proxy get();
+}
