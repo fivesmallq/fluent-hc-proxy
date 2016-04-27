33675417fc8c0e9c3398a2547d166e4a11fe5b27
fivesmallq <fivesmallq@gmail.com>
2016-04-27 13:35:06 +0800

    of method create instance

--- src/main/java/im/nll/data/fluent/Proxies.java
+++ src/main/java/im/nll/data/fluent/Proxies.java
@@ -13,7 +13,48 @@ import java.io.IOException;
  * @date 16/4/11 下午7:13
  */
 public class Proxies {
-    Executor executor = Executor.newInstance();
+    private Proxy proxy;
+    private Executor executor = Executor.newInstance();
+
+    public Proxies(Proxy proxy) {
+        this.proxy = proxy;
+    }
+
+    /**
+     * create proxies instance with proxy.
+     *
+     * @param proxy
+     * @return
+     */
+    public static Proxies of(Proxy proxy) {
+        return new Proxies(proxy);
+    }
+
+    /**
+     * create proxies instance with proxy string.
+     *
+     * @param proxy
+     * @return
+     */
+    public static Proxies of(String proxy) {
+        return new Proxies(Proxy.parse(proxy));
+    }
+
+    public Proxies executor(Executor executor) {
+        this.executor = executor;
+        return this;
+    }
+
+    /**
+     * execute request use default proxy
+     *
+     * @param request
+     * @return
+     * @throws IOException
+     */
+    public Response execute(Request request) throws IOException {
+        return execute(proxy, request);
+    }
 
     public Response execute(String proxy, Request request) throws IOException {
         Proxy p = Proxy.parse(proxy);
@@ -27,4 +68,6 @@ public class Proxies {
         request.viaProxy(httpHost);
         return executor.execute(request);
     }
+
+
 }
---
+++ src/test/java/im/nll/data/fluent/ProxiesTest.java
@@ -0,0 +1,37 @@
+package im.nll.data.fluent;
+
+import org.apache.http.client.fluent.Request;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:fivesmallq@gmail.com">fivesmallq</a>
+ * @version Revision: 1.0
+ * @date 16/4/27 下午1:30
+ */
+public class ProxiesTest {
+    @Test
+    public void of() throws Exception {
+        Proxies.of("").execute(Request.Get(""));
+    }
+
+    @Test
+    public void executor() throws Exception {
+
+    }
+
+    @Test
+    public void execute() throws Exception {
+
+    }
+
+    @Test
+    public void execute1() throws Exception {
+
+    }
+
+    @Test
+    public void execute2() throws Exception {
+
+    }
+
+}
